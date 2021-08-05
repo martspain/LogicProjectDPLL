747ba8a0a96ccde24d1c66652734cc0f8872f474
alejandragudiel <gud19232@uvg.edu.gt>
2021-08-05 01:56:36 -0600

    98% done

--- dpll.py
+++ dpll.py
@@ -3,84 +3,161 @@
 import sys
 from copy import deepcopy
 
-assign_true = set()
-assign_false = set()
-n_props, n_splits = 0, 0
+#ejemplos de la guía
+# p !p
+# q p !p
+# !p !r !s !q !p !s
+# !p !q q !s !p s !q s
+# !p !q !r q !r p !p q r
+# r !q !r !p q !r  q
 
-    
-def solveDPLL(clause, I):
-    trueAssigned = []
-    falseAssigned = []
+truth_assignment = []
+unit_propa = 0
+splitting = -1
 
-    global assign_true, assign_false, n_props, n_splits
-    assign_true = set(assign_true)
-    assign_false = set(assign_false)
-    n_splits += 1
+def remove_liter(cnf):
+    new_liters = list(set(''.join(cnf)))
+    if '!' in new_liters:
+        new_liters.remove('!')
+    if '\n' in new_liters:
+        new_liters.remove('\n')
+    if ' ' in new_liters:
+        new_liters.remove(' ')
+    return new_liters
 
-    clause = list(set(clause))
-    units = [i for i in clause if len(i)<3]
-    units = list(set(units))
-    if len(units):
-        for unit in units:
-            n_props += 1
-            if '!' in unit:
-                assign_false.add(unit[-1])
-                falseAssigned.append(unit[-1])
-                i = 0
-                while True:
-                    if unit in clause[i]:
-                        clause.remove(clause[i])
-                        i -= 1
-                    elif unit[-1] in clause[i]:
-                        clause[i] = clause[i].replace(unit[-1], '').strip()
-                    i += 1
-                    if i >= len(clause):
-                        break
-            else:
-                assign_true.add(unit)
-                trueAssigned.append(unit)
-                i = 0
-                while True:
-                    if '!'+unit in clause[i]:
-                        clause[i] = clause[i].replace('!'+unit, '').strip()
-                        if '  ' in clause[i]:
-                            clause[i] = clause[i].replace('  ', ' ')
-                    elif unit in clause[i]:
-                        clause.remove(clause[i])
-                        i -= 1
-                    i += 1
-                    if i >= len(clause):
-                        break
 
-    if len(clause) == 0:
+def preproc(input_cnf):
+    liters = list(set(input_cnf))
+    if '!' in liters:
+        liters.remove('!')
+    if '\n' in liters:
+        liters.remove('\n')
+    if ' ' in liters:
+        liters.remove(' ')
+    # liters = dict(zip(liters, [False] * len(liters)))
+    liters = list(liters)
+    input_cnf = input_cnf.splitlines()
+
+    return liters, input_cnf
+
+
+def dpll(cnf, liters):
+    global truth_assignment, unit_propa, splitting
+
+    splitting += 1
+
+    # ------------start unit propa-----------------
+    while True:
+        if len(liters) == 1 and len(cnf) > 1:
+            print('--------------------------------------------------')
+            print(cnf)
+            break
+
+        delete_list = []
+        unit_clause = None
+        cnf = list(set(cnf))
+
+        # find unit clause
+        for i in range(len(cnf)):
+            if ' ' not in cnf[i]:
+                unit_clause = cnf[i]
+                truth_assignment.append(unit_clause)
+                del cnf[i]
+                break
+
+        # exception handle
+        if unit_clause:
+            for i in range(len(cnf)):
+                if '!' in unit_clause:
+                    if unit_clause.replace('!', '') == cnf[i]:
+                        return False
+                else:
+                    if '!' + unit_clause == cnf[i]:
+                        return False
+
+        if unit_clause and '!' not in unit_clause:
+            unit_propa += 1
+            for j in range(len(cnf)):
+                if '!' + unit_clause in cnf[j]:
+                    if '!' + unit_clause == cnf[j]:
+                        delete_list.append(j)
+                    else:
+                        cnf[j] = cnf[j].replace('!' + unit_clause, ' ')
+                        cnf[j] = cnf[j].strip(' ')
+                elif unit_clause in cnf[j]:
+                    delete_list.append(j)
+
+            for index in sorted(delete_list, reverse=True):
+                del cnf[index]
+            liters = remove_liter(cnf)
+
+        elif unit_clause and '!' in unit_clause:
+            unit_propa += 1
+            for j in range(len(cnf)):
+                if unit_clause in cnf[j]:
+                    delete_list.append(j)
+                elif unit_clause.replace('!', '') in cnf[j]:
+                    if unit_clause.replace('!', '') == cnf[j]:
+                        delete_list.append(j)
+                    else:
+                        cnf[j] = cnf[j].replace(unit_clause.replace('!', ''), ' ')
+                        cnf[j] = cnf[j].strip(' ')
+
+            for index in sorted(delete_list, reverse=True):
+                del cnf[index]
+            liters = remove_liter(cnf)
+
+        else:
+            break
+
+    # checking clause
+    if cnf == []:
         return True
-
-    if sum(len(clause)==0 for clause in clause):
-        for i in trueAssigned:
-            assign_true.remove(i)
-        for i in falseAssigned:
-            assign_false.remove(i)
+    elif len(liters) == 1 and len(cnf) > 1:
         return False
-    I = [k for k in list(set(''.join(clause))) if k.isalpha()]
 
-    x = I[0]
-    if solveDPLL(deepcopy(clause)+[x], deepcopy(I)):
-        return True
-    elif solveDPLL(deepcopy(clause)+['!'+x], deepcopy(I)):
+    # splitting
+    if dpll(cnf + [liters[0]], deepcopy(liters)) or dpll(cnf + ['!' + liters[0]], deepcopy(liters)):
         return True
     else:
-        for i in trueAssigned:
-            assign_true.remove(i)
-        for i in falseAssigned:
-            assign_false.remove(i)
         return False
 
-clauses = [
-    [{"p"}, {"!p"}],
-    [{"q", "p", "!p"}],
-    [{"!p", "!r", "!s"}, {"!q", "!p", "!s"}],
-    [{"!p", "!q"}, {"q", "!s"}, {"!p", "s"}, {"!q", "s"}],
-    [{"!p", "!q", "!r"}, {"q", "!r", "p"}, {"!p", "q", "r"}],
-    [{"r"}, {"!q", "!r"}, {"!p", "q", "!r"}, {"q"}]
-]
 
+def main():
+    global truth_assignment
+    input_cnf = open("example.txt", 'r').read()
+    liters, cnf = preproc(input_cnf)
+
+    if dpll(cnf, liters):
+        print('--------------------------------------------------')
+        print('[Resultado] : satisfacible')
+
+        # print truth assignment
+        for i in range(len(truth_assignment) - 1, 0, -1):
+            for j in range(i - 1, -1, -1):
+                if '!' in truth_assignment[i]:
+                    if truth_assignment[i] == truth_assignment[j] or truth_assignment[i].replace('!', '') == \
+                            truth_assignment[j]:
+                        truth_assignment[j] = ''
+                else:
+                    if truth_assignment[i] == truth_assignment[j]:
+                        truth_assignment[j] = ''
+
+        if '' in truth_assignment:
+            truth_assignment = list(set(truth_assignment))
+            truth_assignment.remove('')
+
+        #show truth clauses
+        print('[Truth Assignment] | ', end='')
+        for i in truth_assignment:
+            if '!' in i:
+                print(i.replace('!', ''), ' : False', end=' | ')
+            else:
+                print(i, ' : True', end=' | ')
+        print('')
+    else:
+        print('--------------------------------------------------')
+        print('[Resultado] : insatisfacible, {}')
+
+if __name__ == '__main__':
+    main()
